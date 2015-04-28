3a5e62cee7ac0844ac21330bd23d0de7d7ba6e22
Jeremy Alexander <jdoubleua@gmail.com>
2015-04-27 23:16:06 -0400

    fixed base.css

--- public/app.js
+++ public/app.js
@@ -3,18 +3,24 @@ var app = express();
 var jade = require('jade');
 var routes = require('./routes/router.js');
 
+/*
+View Engine
+ */
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 
-//app.get('/', function(req, res){
-//   res.render("pages/index");
-//});
-
+/*
+Routes
+ */
 app.use('/', routes);
 app.use('/login', routes);
-
+/*
+Access public directory
+ */
 app.use('/public', express.static('public'));
-//
+/**
+ * Log server
+ */
 app.listen(3000, function(){
    console.log("SERVER");
 })
