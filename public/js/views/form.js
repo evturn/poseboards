71b8a56dc03ed66926fe12fd5dbead7813b885c8
Evan Turner <evturn@gmail.com>
2015-04-28 02:20:41 -0400

    register saving and redirecting

--- public/js/views/form.js
+++ public/js/views/form.js
@@ -40,35 +40,40 @@ app.Form = Backbone.View.extend({
 	},
 	register: function(e) {
 		e.preventDefault();	
-		console.log('User has registered!');
 		var email = $('#register-email').val();
 		var password = $('#register-password').val();
+		var username = $('#register-username').val();
 		
 		app.ref.createUser({
 		  email: email,
 		  password: password
 		}, function(error, userData) {
+			$('.register-error').css({color: 'red'});
+			$('.register-error').css({fontWeight: 'bold'});
 		  if (error) {
 		    switch (error.code) {
 		      case "EMAIL_TAKEN":
 		        console.log("The new user account cannot be created because the email is already in use.");
+		        $('.register-error').text("The new user account cannot be created because the email is already in use.")
 		        break;
 		      case "INVALID_EMAIL":
 		        console.log("The specified email is not a valid email.");
+		        $('.register-error').text("The specified email is not a valid email.")
 		        break;
 		      default:
 		        console.log("Error creating user:", error);
+		        $('.register-error').text("Error creating user")
 		    }
 		  } else {
 		    console.log("Successfully created user account with uid:", userData.uid);
 		    usersCollection.push({
+		    	username: username,
 		    	email: email,
 		    	password: password,
 		    	refId: userData.uid
-		    })
-		  	
+		    });
+		    window.location = '/';
 		  }
-		  
 		});
 	},
 });
