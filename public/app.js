1292c5669b44b67822507cf101a4e3b18ddce1d5
Jeremy Alexander <jdoubleua@gmail.com>
2015-04-27 21:25:15 -0400

    fixed all errors. make sure you put bower components in the public folder!

--- public/app.js
+++ public/app.js
@@ -2,16 +2,16 @@ var express = require('express');
 var app = express();
 var jade = require('jade');
 
-app.set('views', __dirname + '/views');
-app.engine('html', require('jade').renderFile);
 
-app.set('view engine', 'jade');
-
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
 
 app.get('/', function(req, res){
-   res.render('pages/index.jade');
+   res.render("pages/index");
 });
 
+app.use('/public', express.static('public'));
+
 app.listen(3000, function(){
-   console.log("Server listening on port 3000");
-});
+   console.log("SERVER");
+})
