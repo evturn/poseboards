bd6d9ca930a74323a873750c6c42cc77f7b530b3
Evan Turner <evturn@gmail.com>
2015-04-29 00:15:15 -0400

    fonts readded to css

--- public/js/main.js
+++ public/js/main.js
@@ -5,5 +5,4 @@ var ref 		 = new Firebase("https://poseboards.firebaseio.com");
 var refUsers = new Firebase("https://poseboards.firebaseio.com/users");
 
 // backbone
-new app.App();
-new app.Form();
+new app.Auth();
