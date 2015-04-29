1bde285a70d8fd162b0832ae84d59f7e8de1b6c3
Evan Turner <evturn@gmail.com>
2015-04-29 00:48:53 -0400

    users stored with username in path

--- public/js/main.js
+++ public/js/main.js
@@ -5,4 +5,12 @@ var ref 		 = new Firebase("https://poseboards.firebaseio.com");
 var refUsers = new Firebase("https://poseboards.firebaseio.com/users");
 
 // backbone
-new app.Auth();
+var auth = new app.Auth();
+
+ref.onAuth(function(authData) {
+  if (authData) {
+    console.log("Authenticated with uid:", authData.uid);
+  } else {
+    console.log("Client unauthenticated.")
+  }
+});
--- public/js/views/app.Auth.js
+++ public/js/views/app.Auth.js
@@ -3,13 +3,43 @@ var app = app || {};
 app.Auth = Backbone.View.extend({
 	el: '.app-container',
 	events: {
-		'click .btn-login' 		: 'login',
 		'click .btn-register' : 'register'
 	},
 	register: function(e) {
 		e.preventDefault();	
-		var email = $('#register-email').val();
+		var email 	 = $('#register-email').val();
 		var password = $('#register-password').val();
-		var username = $('#register-username').val();
+		var user = ref.createUser({
+			email: email,
+			password: password
+		}, function(error, userData) {
+		  if (error) {
+		    switch (error.code) {
+		      case "EMAIL_TAKEN":
+		        console.log("The new user account cannot be created because the email is already in use.");
+		        $('.register-error').text("The new user account cannot be created because the email is already in use.")
+		        break;
+		      case "INVALID_EMAIL":
+		        console.log("The specified email is not a valid email.");
+		        $('.register-error').text("The specified email is not a valid email.")
+		        break;
+		      default:
+		        console.log("Error creating user:", error);
+		        $('.register-error').text("Error creating user")
+		    }
+		  } else {
+		    console.log("Successfully created user account with uid:", userData.uid);
+		    var username = $('#register-username').val();
+		    var ref = new Firebase("https://poseboards.firebaseio.com/users/" + username);
+		    var user = ref.push({
+		    	email: email,
+		    	password: password,
+		    	uid: userData.uid
+		    });
+		    
+		    console.log(user.key());
+		    // window.location = '/login';
+		  }
+		})
 	},
 });
